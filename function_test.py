dcf8ae003e12938ff9359b0f4f17c17b5c6b7e77
GabrielDelcominette <150939579+GabrielDelcominette@users.noreply.github.com>
2023-12-20 16:47:46 +0100

    fonction pertinent_file et remove_useless_words_from_matrice et ça marche cette fois

---
+++ function_test.py
@@ -0,0 +1,7 @@
+# ***** choix du répertoire à traiter *****
+directory = "./speeches"
+cleaned_directory = directory+"_cleaned"
+files_name_list = list_of_files(directory, "txt")
+
+# ***** TEST DES FONCTIONNALITES *****
+
